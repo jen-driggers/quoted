3a25cb2b285580337ad631b74101b8ba682edb7d
Jennifer Driggers <jenniferdriggers@Jennifers-MBP.domain>
2016-06-11 13:32:09 -0700

    refactor to use object properties instead of just strings

---
+++ Quoted/Quote.swift
@@ -0,0 +1,19 @@
+//
+//  Quote.swift
+//  Quoted
+//
+//  Created by Jennifer Driggers on 6/11/16.
+//  Copyright © 2016 jennyd.info. All rights reserved.
+//
+
+import UIKit
+
+class Quote: NSObject {
+    
+    var imageName: String
+    
+    init(imageName: String) {
+        self.imageName = imageName
+    }
+
+}
--- Quoted/ViewController.swift
+++ Quoted/ViewController.swift
@@ -15,7 +15,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var quoteView: UIImageView!
     
-    var quotesArray:[String] = ["grendel","catcher","design","stranger","alchemist","vendetta","watchmen","bfg","alice","grapes","naked","neuromancer","thinking","voice","american","good"]
+//    var quotesArray:[String] = ["grendel","catcher","design","stranger","alchemist","vendetta","watchmen","bfg","alice","grapes","naked","neuromancer","thinking","voice","american","good"]
+    
+    var quotesArray =
+        [Quote(imageName: "grendel"),
+         Quote(imageName: "catcher"),
+         Quote(imageName: "design"),
+         Quote(imageName: "stranger"),
+         Quote(imageName: "alchemist"),
+         Quote(imageName: "vendetta"),
+         Quote(imageName: "watchmen"),
+         Quote(imageName: "bfg"),
+         Quote(imageName: "alice"),
+         Quote(imageName: "grapes"),
+         Quote(imageName: "naked"),
+         Quote(imageName: "neuromancer"),
+         Quote(imageName: "thinking"),
+         Quote(imageName: "voice"),
+         Quote(imageName: "american"),
+         Quote(imageName: "good"),]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,12 +49,11 @@ class ViewController: UIViewController {
         
         let randomNumber = Int(arc4random_uniform(UInt32(quotesArray.count)))
         
-        let imageName:String = quotesArray[randomNumber]
+        let quote = quotesArray[randomNumber]
         
-        quoteView.image = UIImage(named: imageName)
+        quoteView.image = UIImage(named: quote.imageName)
     }
 
-    
 
 }
 
