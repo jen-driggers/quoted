d8e1d48dd65cde6192a6e90601d62a1687d9aa9f
Jennifer Driggers <jenniferdriggers@Jennifers-MBP.domain>
2016-06-24 15:08:04 -0700

    add unique id's to each quote

--- Quoted/Quote.swift
+++ Quoted/Quote.swift
@@ -11,9 +11,18 @@ import UIKit
 class Quote: NSObject {
     
     var imageName: String
+    let uid: Int
+    static var nextUid = 0
+    static func generateUid() -> Int {
+        nextUid += 1
+        return nextUid
+    }
     
     init(imageName: String) {
         self.imageName = imageName
+        self.uid = Quote.generateUid()
     }
 
 }
+
+
