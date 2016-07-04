07ae1969fef93247f02e5a3d5eafd765bd107b00
Jennifer Driggers <jenniferdriggers@Jennifers-MBP.domain>
2016-07-03 19:40:36 -0700

    bring back favorites class

---
+++ Quoted/Favorites.swift
@@ -0,0 +1,34 @@
+//
+//  Favorites.swift
+//  Quoted
+//
+//  Created by Jennifer Driggers on 6/25/16.
+//  Copyright © 2016 jennyd.info. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Favorites: NSManagedObject {
+    
+    //TODO: figure out how to initialize this class with the Quote id
+
+//    var id: NSInteger?
+//    
+//    init(id: NSInteger?) {
+//        self.id = id
+//    }
+    
+//    required init(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+//    class func newInstance(id: Int, context: NSManagedObjectContext) -> Favorites {
+//        var favorites = NSEntityDescription.insertNewObjectForEntityForName("Favorites",
+//                                                                       inManagedObjectContext: context) as! Favorites
+//        favorites.id
+//        return favorites
+    //}
+
+}
